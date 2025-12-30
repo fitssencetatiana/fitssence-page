7767b7edb85798dd948be32605ab985cfa3f77d3
rrfitnessricardo <faldana4@misena.edu.co>
2025-12-29 21:52:59 -0500

    fix CEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'FITSSENCE',
+  title: 'FITSSENCE By Kfit',
   description: 'Entrenamiento personalizado y bienestar integral con Tatiana',
   generator: 'v0.dev',
   icons: {
